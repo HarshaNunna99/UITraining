7d0ce6d0aa2a93db1e2d35398ae88e7324d3950e
Nunna, Harsha <Harsha.Nunna@in.pega.com>
2020-09-11 08:56:28 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import {LoginComponent} from './components/Login';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = ()=>{
+    const companyName='Pega Systems';
+   return <div>
+       <Header title={companyName} />
+       <LoginComponent />
+       <Footer />
+       </div>;
+};
 
 export default App;
+
+//This is pure component or stateless component
